0b6b859977612672605d9b88f1fc6f6240a500a8
João Vitor Marques <jvsilvam@outlook.com>
2022-12-14 11:03:53 -0300

    :sparkles: feat: create kitchen repository

---
+++ src/main/java/com/myfood/myfood/domain/repository/KitchenRepository.java
@@ -0,0 +1,17 @@
+package com.myfood.myfood.domain.repository;
+
+import java.util.List;
+
+import com.myfood.myfood.domain.model.Kitchen;
+
+public interface KitchenRepository {
+  
+  List<Kitchen> findAll();
+
+  Kitchen findById(Long id);
+
+  Kitchen save(Kitchen kitchen);
+
+  void remove(Kitchen kitchen);
+
+}
---
+++ src/main/java/com/myfood/myfood/infrastructure/repository/KitchenRepositoryImpl.java
@@ -0,0 +1,45 @@
+package com.myfood.myfood.infrastructure.repository;
+
+import java.util.List;
+
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.myfood.myfood.domain.model.Kitchen;
+import com.myfood.myfood.domain.repository.KitchenRepository;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+
+@Component
+public class KitchenRepositoryImpl implements KitchenRepository {
+
+  @PersistenceContext
+  private EntityManager manager;
+
+  @Override
+  public List<Kitchen> findAll() {
+    return manager.createQuery("from Kitchen", Kitchen.class).getResultList();
+  }
+
+  @Override
+  public Kitchen findById(Long id) {
+    return manager.find(Kitchen.class, id);
+  }
+
+  @Transactional
+  @Override
+  public Kitchen save(Kitchen kitchen) {
+    return manager.merge(kitchen);
+  }
+
+  @Transactional
+  @Override
+  public void remove(Kitchen kitchen) {
+    kitchen = findById(kitchen.getId());
+    manager.remove(kitchen);
+  }
+  
+  
+
+}
