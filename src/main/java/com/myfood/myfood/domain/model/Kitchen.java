7a9a4c09c94f95d21758d0c8677e437a4bb4cd3c
João Vitor Marques <jvsilvam@outlook.com>
2022-12-14 08:41:51 -0300

    :sparkles: feat: add auto increment to id

--- src/main/java/com/myfood/myfood/domain/model/Kitchen.java
+++ src/main/java/com/myfood/myfood/domain/model/Kitchen.java
@@ -2,12 +2,15 @@ package com.myfood.myfood.domain.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 
 @Entity
 public class Kitchen {
   
   @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
   private Long id;
 
   @Column
